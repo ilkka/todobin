89a8f4d99a8cda6ed4b484e823641acec0eabb0d
Ilkka Laukkanen <ilkka.laukkanen@sasken.com>
2011-05-05 14:54:45 +0300

    Add settings object as a context property

--- main.cpp
+++ main.cpp
@@ -1,11 +1,16 @@
 #include <QtGui/QApplication>
+#include <QDeclarativeContext>
 #include "qmlapplicationviewer.h"
+#include "settings.h"
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
 
+    Settings settings;
+
     QmlApplicationViewer viewer;
+    viewer.rootContext()->setContextProperty("settings", &settings);
     viewer.setOrientation(QmlApplicationViewer::ScreenOrientationAuto);
     viewer.setMainQmlFile(QLatin1String("qml/RTMApp/main.qml"));
     viewer.showExpanded();
