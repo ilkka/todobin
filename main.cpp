125832dc19322fab01a1cfc7ffc005f1122098e8
Ilkka Laukkanen <ilkka.s.laukkanen@gmail.com>
2011-05-06 12:22:39 +0300

    Create tasks model in C++ and pass to application

--- main.cpp
+++ main.cpp
@@ -4,18 +4,21 @@
 
 #include "qmlapplicationviewer.h"
 #include "settings.h"
+#include "tasksmodel.h"
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
 
     Settings settings;
-
     // Hardcode endpoint URL here
     settings.setValue("foursquare/endpoint", QUrl("http://api.rememberthemilk.com/services/rest/"));
 
+    TasksModel model;
+
     QmlApplicationViewer viewer;
     viewer.rootContext()->setContextProperty("settings", &settings);
+    viewer.rootContext()->setContextProperty("tasksModel", &model);
     viewer.setOrientation(QmlApplicationViewer::ScreenOrientationAuto);
     viewer.setMainQmlFile(QLatin1String("qml/RTMApp/main.qml"));
     viewer.showExpanded();
