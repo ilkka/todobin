9d9c4c9d6d2f2056b6b43bed23a27b1faec0ae10
Ilkka Laukkanen <ilkka.s.laukkanen@gmail.com>
2011-05-08 15:52:11 +0300

    Add tags, due date to Task class

--- task.h
+++ task.h
@@ -2,16 +2,29 @@
 #define TASK_H
 
 #include <QObject>
+#include <QDateTime>
+#include <QStringList>
 
 class Task : public QObject
 {
     Q_OBJECT
     Q_PROPERTY(QString title READ title WRITE setTitle NOTIFY titleChanged)
+    Q_PROPERTY(QDateTime due READ due WRITE setDue NOTIFY dueChanged)
+    Q_PROPERTY(QStringList tags READ tags WRITE setTags NOTIFY tagsChanged)
 public:
     explicit Task(QObject *parent = 0);
 
+    Q_INVOKABLE void addTag(const QString& tag) {
+        if (!m_tags.contains(tag)) {
+            m_tags << tag;
+            emit tagsChanged();
+        }
+    }
+
     // getters
     const QString& title() const { return m_title; }
+    const QDateTime& due() const { return m_due; }
+    const QStringList& tags() const { return m_tags; }
 
     // setters
     void setTitle(const QString& title) {
@@ -20,13 +33,28 @@ public:
             emit titleChanged();
         }
     }
+    void setDue(const QDateTime& due) {
+        if (m_due != due) {
+            m_due = due;
+            emit dueChanged();
+        }
+    }
+    void setTags(const QStringList& tags) {
+        if (m_tags != tags) {
+            m_tags = tags;
+            emit tagsChanged();
+        }
+    }
 
 signals:
     void titleChanged();
+    void dueChanged();
+    void tagsChanged();
 
 private:
     QString m_title;
-
+    QDateTime m_due;
+    QStringList m_tags;
 };
 
 #endif // TASK_H
