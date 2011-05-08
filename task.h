15c15ff94ab5ef3e269a05773be67d6b530351ec
Ilkka Laukkanen <ilkka.s.laukkanen@gmail.com>
2011-05-08 17:28:16 +0300

    Add priority and completion data to Task class

--- task.h
+++ task.h
@@ -11,7 +11,19 @@ class Task : public QObject
     Q_PROPERTY(QString title READ title WRITE setTitle NOTIFY titleChanged)
     Q_PROPERTY(QDateTime due READ due WRITE setDue NOTIFY dueChanged)
     Q_PROPERTY(QStringList tags READ tags WRITE setTags NOTIFY tagsChanged)
+    Q_ENUMS(Priority)
+    Q_PROPERTY(Priority priority READ priority WRITE setPriority NOTIFY priorityChanged)
+    Q_PROPERTY(QDateTime completed READ completed NOTIFY completedChanged)
+    Q_PROPERTY(bool isCompleted READ isCompleted NOTIFY isCompletedChanged)
 public:
+    // valid task priorities
+    enum Priority {
+        PRIO_NONE,
+        PRIO_FIRST,
+        PRIO_SECOND,
+        PRIO_THIRD
+    };
+
     explicit Task(QObject *parent = 0);
 
     Q_INVOKABLE void addTag(const QString& tag) {
@@ -25,6 +37,9 @@ public:
     const QString& title() const { return m_title; }
     const QDateTime& due() const { return m_due; }
     const QStringList& tags() const { return m_tags; }
+    Priority priority() const { return m_priority; }
+    const QDateTime& completed() const { return m_completed; }
+    bool isCompleted() const { return m_completed.isValid(); }
 
     // setters
     void setTitle(const QString& title) {
@@ -45,16 +60,37 @@ public:
             emit tagsChanged();
         }
     }
+    void setPriority(Priority priority) {
+        if (m_priority != priority) {
+            m_priority = priority;
+            emit priorityChanged();
+        }
+    }
+    void setCompleted(const QDateTime& completed) {
+        if (m_completed != completed) {
+            bool changes = (m_completed.isValid() != completed.isValid());
+            m_completed = completed;
+            emit completedChanged();
+            if (changes) {
+                emit isCompletedChanged();
+            }
+        }
+    }
 
 signals:
     void titleChanged();
     void dueChanged();
     void tagsChanged();
+    void priorityChanged();
+    void completedChanged();
+    void isCompletedChanged();
 
 private:
     QString m_title;
     QDateTime m_due;
     QStringList m_tags;
+    Priority m_priority;
+    QDateTime m_completed;
 };
 
 #endif // TASK_H
