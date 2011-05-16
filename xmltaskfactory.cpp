2143326ec64c3a317188358b32386b0e82a95ff3
Ilkka Laukkanen <ilkka.laukkanen@sasken.com>
2011-05-16 16:34:09 +0300

    Simplify taskfactory XPath to only look for series in lists
    This allows the same XPath expression to be used for other types of RTM API
    replies returning task info.

--- xmltaskfactory.cpp
+++ xmltaskfactory.cpp
@@ -68,7 +68,7 @@ void XmlTaskFactory::setSource(QIODevice *source)
 {
     // Build and evaluate query
     d->query.bindVariable("input", source);
-    d->query.setQuery("doc($input)/rsp/tasks/list/taskseries");
+    d->query.setQuery("doc($input)//list/taskseries");
     d->query.evaluateTo(this);
 }
 
